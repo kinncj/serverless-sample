364eb8a8f7b3a7c3ba15ded0195aa202bef6d10f
Kinn Coelho Julião <kinncj@php.net>
2016-05-13 23:06:00 -0400

    Useless improvements

--- restApi/consulta/consulta.js
+++ restApi/consulta/consulta.js
@@ -3,47 +3,44 @@ import request from 'request';
 
 export default class Consulta
 {
-    constructor(termo, cb, cache)
+    constructor(term, cb, cache)
     {
         this.cb    = cb;
         this.cache = cache;
-        this.termo = termo;
+        this.term  = term;
 
-        let entry = this.cache.get(termo);
+        let entry = this.cache.get(term);
 
         if (entry) {
-            console.log("Cache found for: " + termo);
-
             cb(null, entry);
+
             return;
         }
 
         request.get({
-            "url": `http://dados.gov.br/dataset?q=${termo}`,
+            "url": `http://dados.gov.br/dataset?q=${term}`,
             "encoding": null
         }, this.scrape.bind(this));
     }
 
     scrape(error, response, html) {
         if (error) {
-            console.log('errorCB:', this.cb(error));
+            this.cb(error);
             throw error;
         }
 
         let parsed = $.load(html),
-            links  = [];
-
-        parsed('#content > div.wrapper > div > section:nth-child(1) > div > ul > li > .dataset-content').map(function(i, data) {
-            links.push({
-                'text':   $(this).find('div').text(),
-                'header': $(this).find('a').text(),
-                'link':   $(this).find('a').attr('href')
-            });
+            links  = parsed('#content > div.wrapper > div > section:nth-child(1) > div > ul > li > .dataset-content')
+        .map(function(i, data) {
+            return {
+                "text":   $(this).find('div').text(),
+                "header": $(this).find('a').text(),
+                "link":   $(this).find('a').attr('href')
+            };
         });
 
-        this.cache.set(this.termo, links);
-
-        console.log('successCB:', this.cb(null, links));
+        this.cache.set(this.term, links);
+        this.cb(null, links);
     }
 
     static create(event, cb, cache)
